85f72f4511ca39e657333f9e24f66cefe7d5db66
Niko Virtala <nikovirtala@users.noreply.github.com>
2021-05-14 09:20:38 +0300

    chore: update cdk and projen. (#92)

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.102.0',
+  cdkVersion: '1.103.0',
   name: 'cdk-fargate-apigateway-http-api',
   cdkDependencies: [
     '@aws-cdk/aws-ec2',

2a292c53ca9cff195059af72ea01745664bb8199
Niko Virtala <niko.virtala@hey.com>
2022-03-22 12:48:20 +0200

    chore(deps): upgrade dependencies

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { awscdk } = require('projen');
 
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.3.0',
+  cdkVersion: '2.17.0',
   name: 'cdk-fargate-apigateway-http-api',
   context: { '@aws-cdk/core:newStyleStackSynthesis': true },
   authorEmail: 'niko.virtala@hey.com',
@@ -12,7 +12,10 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   buildWorkflow: true,
   codeCov: false,
   defaultReleaseBranch: 'main',
-  deps: ['@aws-cdk/aws-apigatewayv2-alpha@2.3.0-alpha.0', '@aws-cdk/aws-apigatewayv2-integrations-alpha@2.3.0-alpha.0'],
+  deps: [
+    '@aws-cdk/aws-apigatewayv2-alpha@2.17.0-alpha.0',
+    '@aws-cdk/aws-apigatewayv2-integrations-alpha@2.17.0-alpha.0',
+  ],
   dependabot: false,
   depsUpgradeOptions: {
     ignoreProjen: false,
